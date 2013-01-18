105d3c0349e78823ae4496c68b5223f741c7814d
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-01-18 18:24:57 -0500

    required functions-p in functions

--- functions.php
+++ functions.php
@@ -1,8 +1,16 @@
 /*-----------------------------------------------------------------------------------*/
 /*	Include functions
 /*-----------------------------------------------------------------------------------*/
+
+//FROM PRONTO
+
+//main pronto functions
+require('functions/functions-p.php');
+//other pronto functions
 require('admin/theme-admin.php');
 require('functions/pagination.php');
 require('functions/better-excerpts.php');
 require('functions/shortcodes.php');
 require('functions/flickr-widget.php');
+
+//FROM FOUNDATION
