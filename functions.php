7a345cbc2ec1745dbb80246bb78d9786df560467
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-01-18 18:23:22 -0500

    created a functions file just to include things in functions folder.

---
+++ functions.php
@@ -0,0 +1,8 @@
+/*-----------------------------------------------------------------------------------*/
+/*	Include functions
+/*-----------------------------------------------------------------------------------*/
+require('admin/theme-admin.php');
+require('functions/pagination.php');
+require('functions/better-excerpts.php');
+require('functions/shortcodes.php');
+require('functions/flickr-widget.php');
--- functions/functions-p.php
+++ functions/functions-p.php
@@ -11,14 +11,6 @@ if ( ! isset( $content_width ) )
 
 
 
-/*-----------------------------------------------------------------------------------*/
-/*	Include functions
-/*-----------------------------------------------------------------------------------*/
-require('admin/theme-admin.php');
-require('functions/pagination.php');
-require('functions/better-excerpts.php');
-require('functions/shortcodes.php');
-require('functions/flickr-widget.php');
 
 
 
