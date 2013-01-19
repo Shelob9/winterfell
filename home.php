595e5afcb1ee712d4ee839ed1c9395f6eddac28d
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-01-19 18:38:00 -0500

    proper functions for home page dynamic sidebar

--- home.php
+++ home.php
@@ -13,11 +13,11 @@ get_header(); ?>
 <?php $options = get_option( 'Pronto_theme_settings' ); ?>
     <!-- Main Content -->
     <div class="row" id="home-top">
-    	<div class="two columns" id="home-social">
+    	<div class="three columns" id="home-social">
 			<?php get_template_part( 'social'); ?>
 		</div>
     
-    	<div class="ten columns offset-by-one" id="home-orbit">
+    	<div class="nine columns" id="home-orbit">
 			<?php get_template_part( 'orbit'); ?>
 		</div>
 		
@@ -27,42 +27,47 @@ get_header(); ?>
     
     <!-- End Main Content -->
 <!-- home footer -->
-	<div class="row" id="home-footer-widgets">
-	<?php if ( dynamic_sidebar('Sidebar Footer One') && dynamic_sidebar('Sidebar Footer Two') && dynamic_sidebar('Sidebar Footer Three') && dynamic_sidebar('Sidebar Footer Four')  ) : else : ?>
+<div class="row" id="home-footer-widgets">
 
 	<div class="twelve columns" id="home-footer-widgets">
+		
+			<div class="three columns offset-by-one home-widget">
+				<?php if ( dynamic_sidebar('Home Widget One')  ) : else : ?>
+					<h5>Pages</h5>
+					<ul>
+					<?php wp_list_pages('title_li='); ?>
+					</ul>
+				<?php endif ?>
+			</div>
+		
 		<div class="three columns offset-by-one home-widget">
-				<h5>Pages</h5>
-				<ul>
-				<?php wp_list_pages('title_li='); ?>
-				</ul>
-		</div>
-
-		<div class="three columns offset-by-one home-widget">
-			<h5>Tags</h5>
+			<?php if ( dynamic_sidebar('Home Widget Two')  ) : else : ?>
+				<h5>Tags</h5>
 				<?php
 				if(get_the_tag_list()) {
 					echo get_the_tag_list('<p><span class="secondary label radius">','</span>&nbsp;<span class="secondary label radius">','</span></p>');
 				}
 				?>
-			
+			<?php endif ?>
 		</div>
 
 		<div class="three columns offset-by-one home-widget">
-			<h5>Recent Posts</h5>
-				<ul>
-				<?php
-				$recent_posts = wp_get_recent_posts();
-				foreach( $recent_posts as $recent ){
-					echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
-					}
-				?>
-				</ul>
+			<?php if ( dynamic_sidebar('Home Widget Two')  ) : else : ?>
+				<h5>Recent Posts</h5>
+					<ul>
+					<?php
+					$recent_posts = wp_get_recent_posts();
+					foreach( $recent_posts as $recent ){
+						echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
+						}
+					?>
+					</ul>
+			<?php endif ?>
 		</div>
 	</div>
 
-<?php endif; ?>
-	</div>
+
+</div>
 
 
 <?php wp_footer(); ?>
