51b20bf2f1a5ac53eea09dd79cf1f4dfb6f9f152
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-01-19 17:15:49 -0500

    orbit loop

--- orbit.php
+++ orbit.php
@@ -1,4 +1,28 @@
+<?php
+/**
+ * Content
+ *
+ * Displays content shown in the 'index.php' loop, default for 'standard' post format
+ *
+ * @package WordPress
+ * @subpackage Foundation, for WordPress
+ * @since Foundation, for WordPress 1.0
+ */
+?>
+<!-- ORBIT --!>
+<?php if ( have_posts() ) : ?>
+<?php query_posts('category_name=special_cat&posts_per_page=10'); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
+		
+	<?php if ( has_post_thumbnail('orbit-slide')) : ?>
+	<a href="<?php the_permalink(); ?>" class="th" title="<?php the_title_attribute(); ?>" ><?php the_post_thumbnail(); ?></a>
+	<?php endif; ?>
+
+	<?php the_excerpt(); ?>
+	
+	<?php endwhile ?>
+		</div>
 
 
 
