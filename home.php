dca63921175bacb6a3b24f6ea6b57958300ae88c
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-01-19 17:23:20 -0500

    skeleton home

---
+++ home.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Index
+ *
+ * Standard loop for the front-page
+ *
+ * @package WordPress
+ * @subpackage Foundation, for WordPress
+ * @since Foundation, for WordPress 1.0
+ */
+
+get_header(); ?>
+<?php $options = get_option( 'Pronto_theme_settings' ); ?>
+    <!-- Main Content -->
+    
+    
+    
+    <!-- End Main Content -->
+
+
+<?php get_footer(); ?>
