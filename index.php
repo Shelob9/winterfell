e79d440576be7edbbf3d4e747997875f805ccdc3
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-01-20 00:29:11 -0500

    darkly

--- index.php
+++ index.php
@@ -1,51 +1,22 @@
 <?php
 /**
- * Index
- *
- * Standard loop for the front-page
- *
  * @package WordPress
- * @subpackage Foundation, for WordPress
- * @since Foundation, for WordPress 1.0
+ * @subpackage Pronto Theme
  */
+$options = get_option( 'Pronto_theme_settings' );
+?>
+<?php get_header(' '); ?>
 
-get_header(); ?>
-<?php $options = get_option( 'Pronto_theme_settings' ); ?>
-    <!-- Main Content -->
-    <div class="nine columns" role="content">
-
-		<!-- masonry-wrap -->
 <div id="masonry-wrap">
 
-	<?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>  
-			<?php if(has_post_thumbnail() ) { ?>
-			<div class="loop-entry">
-					<div class="loop-entry-thumbnail">
-						<a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('post-thumb'); ?></a>
-					</div>
-				<div class="loop-entry-details">
-					<h2><a href="<?php the_permalink(' ') ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-					<?php  echo excerpt('15'); ?>
-					<div class="loop-entry-cat">
-						<?php _e('Posted In', 'pronto'); ?>: <?php the_category(' '); ?>
-					</div>
-				</div>
-				<!-- END loop-entry-details -->  
-			</div><!-- END entry -->
-
-			<?php } ?>
-			<?php endwhile; ?>
-			
+	<?php 
+    if (have_posts()) :
+        get_template_part( 'loop' , 'entry');
+    endif;
+    ?>
 
 </div>  
 <!-- /masonry-wrap -->
 
-		<?php foundation_pagination(); ?>
-		<?php endif ?>
-
-    </div>
-    <!-- End Main Content -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php if (function_exists("pagination")) { pagination(); } ?>
+<?php get_footer(' '); ?>
