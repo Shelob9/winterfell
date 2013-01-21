2ec7bb148fc178a6612c9e7891aa7aeebe1438a4
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-01-20 19:30:42 -0500

    footer copyright, powered by, and shit

--- footer.php
+++ footer.php
@@ -14,18 +14,29 @@
 <!-- End Page -->
 
 <!-- Footer -->
-<footer class="row">
-	<div class="twelve columns panel radius callout">
-		footer
-		<?php if ( dynamic_sidebar('Sidebar Footer One') && dynamic_sidebar('Sidebar Footer Two') && dynamic_sidebar('Sidebar Footer Three') && dynamic_sidebar('Sidebar Footer Four')  ) : else : ?>
-
-		<div class="twelve columns">
-			<ul class="link-list">
-				<?php wp_list_bookmarks('categorize=0&title_li='); ?>
-			</ul>
+<footer>
+	<div class="row">
+		<div class="twelve columns panel radius callout">
+			<div class="row">
+				<?php if ( dynamic_sidebar('Sidebar Footer One') && dynamic_sidebar('Sidebar Footer Two') && dynamic_sidebar('Sidebar Footer Three') && dynamic_sidebar('Sidebar Footer Four')  ) : else : ?>
+				<div class="twelve columns">
+					<ul class="link-list">
+						<?php wp_list_bookmarks('categorize=0&title_li='); ?>
+					</ul>
+				</div>
+				<?php endif; ?>
+			</div>
+	
+	
+			<div class="row">
+				<div class="eight columns centered">
+					<?php echo comicpress_copyright(); ?> by
+					<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>.
+					&#8226; Powered by <a href="http://WordPress.org" title="WordPress">WordPress</a>
+					&#8226; <a href="http://ComplexWaveform.com/Winterfell" Title="Winterfell WordPress Theme: Winter Is Coming...">Winterfell Theme by <a href="http://ComplexWaveform.com" Title="Josh Pollock: ComplexwaveForm">Josh Pollock</a>.
+				</div>
+			</div>
 		</div>
-
-		<?php endif; ?>
 	</div>
 </footer>
 <!-- End Footer -->
--- functions.php
+++ functions.php
@@ -78,7 +78,31 @@ function after_setup_theme(){
 			add_image_size( 'orbit-slide', 540, 450, true ); 
 			add_image_size( 'orbit-slide-small', 100, 83, true ); 
 		}
-
-
+/*-----------------------------------------------------------------------------------*/
+/*	The Others
+/*-----------------------------------------------------------------------------------*/
+//Dynamic Copyright For Footer
+//http://www.wpbeginner.com/wp-tutorials/how-to-add-a-dynamic-copyright-date-in-wordpress-footer/
+function comicpress_copyright() {
+global $wpdb;
+$copyright_dates = $wpdb->get_results("
+SELECT
+YEAR(min(post_date_gmt)) AS firstdate,
+YEAR(max(post_date_gmt)) AS lastdate
+FROM
+$wpdb->posts
+WHERE
+post_status = 'publish'
+");
+$output = '';
+if($copyright_dates) {
+$copyright = "&copy; " . $copyright_dates[0]->firstdate;
+if($copyright_dates[0]->firstdate != $copyright_dates[0]->lastdate) {
+$copyright .= '-' . $copyright_dates[0]->lastdate;
+}
+$output = $copyright;
+}
+return $output;
+}
 
 ?>
