cfd86aae317c272ac3487102abe33b829180adac
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-02-01 01:12:36 -0500

    Added 404: Winter is coming

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+   <div class="nine columns panel callout radius">
+     
+     <h2 class="center">The Page You Requested Can Not Be Found</h2>
+     <h5 class="center"><em>Winter is coming</em></h5>
+     <p>Please contact <a href="mailto:<?php blog_info('email'); ?>">the webmaster</a> to remedy the situation.</p>
+     
+   </div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
