6674b8f0e6aa25dec828d7cbf5bf9d442b0bbec6
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-02-02 01:40:58 -0500

    strip menu support from functions-f

--- functions/functions-f.php
+++ functions/functions-f.php
@@ -24,68 +24,6 @@ add_action( 'after_setup_theme', 'foundation_setup' );
 
 
 
-/**
- * Register Navigation Menus
- */
-
-// Register wp_nav_menus
-function foundation_menus() {
-
-	register_nav_menus(
-		array(
-			'header-menu' => __( 'Header Menu', 'foundation' )
-		)
-	);
-	
-}
-add_action( 'init', 'foundation_menus' );
-
-// Create a graceful fallback to wp_page_menu
-function foundation_page_menu() {
-
-	$args = array(
-	'sort_column' => 'menu_order, post_title',
-	'menu_class'  => 'twelve columns',
-	'include'     => '',
-	'exclude'     => '',
-	'echo'        => true,
-	'show_home'   => false,
-	'link_before' => '',
-	'link_after'  => ''
-	);
-
-	wp_page_menu($args);
-
-}
-
-/**
- * Navigation Menu Adjustments
- */
-
-
-// Add class to navigation sub-menu
-class foundation_navigation extends Walker_Nav_Menu {
-
-function start_lvl(&$output, $depth) {
-	$indent = str_repeat("\t", $depth);
-	$output .= "\n$indent<ul class=\"flyout\">\n";
-}
-
-function display_element( $element, &$children_elements, $max_depth, $depth=0, $args, &$output ) {
-	$id_field = $this->db_fields['id'];
-	if ( !empty( $children_elements[ $element->$id_field ] ) ) {
-		$element->classes[] = 'has-flyout';
-	}
-		Walker_Nav_Menu::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
-	}
-}
-
-// Add a class to the wp_page_menu fallback
-function foundation_page_menu_class($ulclass) {
-	return preg_replace('/<ul>/', '<ul class="nav-bar">', $ulclass, 1);
-}
-
-add_filter('wp_page_menu','foundation_page_menu_class');
 
 /**
  * Create pagination
