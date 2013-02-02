0faa4dcd97477f942edf8a028a670c94061e72d7
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-02-02 02:18:18 -0500

    the_content color:#24221f

--- content-single.php
+++ content-single.php
@@ -21,9 +21,9 @@
 		<?php if ( has_post_thumbnail()) : ?>
 		<a href="<?php the_permalink(); ?>" class="th" title="<?php the_title_attribute(); ?>" ><?php the_post_thumbnail(); ?></a>
 		<?php endif; ?>
-	
-		<?php the_content(); ?>
-
+		<span style="color:#24221f;">
+			<?php the_content(); ?>
+		</span>
 		<footer>
 
 			<p><?php wp_link_pages(); ?></p>
