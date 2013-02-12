38d4db8997847ddf0cae0da031b95352544be3d9
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-02-12 00:12:19 -0500

    got rid of special home page widgets

--- footer.php
+++ footer.php
@@ -22,57 +22,61 @@
 	<div class="row">
 		
 		<div class="twelve columns">
-		<?php if (  !is_home() ) {?>
-			<div class="row">
-				<?php if ( dynamic_sidebar('Sidebar Footer One') && dynamic_sidebar('Sidebar Footer Two') && dynamic_sidebar('Sidebar Footer Three') && dynamic_sidebar('Sidebar Footer Four')  ) : else : ?>
-				<div class="twelve columns">
-					<ul class="link-list">
-						<?php wp_list_bookmarks('categorize=0&title_li='); ?>
-					</ul>
-				</div>
-				<?php endif; ?>
-			</div>
-		<?php }?>
-		<?php if (  is_home() ) {?>
-		<div class="row" id="home-footer-widgets">
-				<div class="four columns   panel radius">
-					<?php if ( dynamic_sidebar('Home Widget One')  ) : else : ?>
-						<h5>Pages</h5>
-						<ul>
-						<?php wp_list_pages('title_li='); ?>
-						</ul>
+		
+		
+			<div class="row" id="footer-widgets">
+					<div class="three columns   panel radius">
+						<?php if ( dynamic_sidebar('Sidebar Footer One')  ) : else : ?>
+							<h5>Pages</h5>
+							<ul>
+							<?php wp_list_pages('title_li='); ?>
+							</ul>
+						<?php endif ?>
+					</div>
+	
+				<div class="three columns  panel radius">
+					<?php if ( dynamic_sidebar('Sidebar Footer Two')  ) : else : ?>
+						<h5>Tags</h5>
+						<?php
+						if(get_the_tag_list()) {
+							echo get_the_tag_list('<p><span class="secondary label radius">','</span>&nbsp;<span class="secondary label radius">','</span></p>');
+						}
+						?>
 					<?php endif ?>
 				</div>
-		
-			<div class="four columns  panel radius">
-				<?php if ( dynamic_sidebar('Home Widget Two')  ) : else : ?>
-					<h5>Tags</h5>
-					<?php
-					if(get_the_tag_list()) {
-						echo get_the_tag_list('<p><span class="secondary label radius">','</span>&nbsp;<span class="secondary label radius">','</span></p>');
-					}
-					?>
-				<?php endif ?>
-			</div>
 
-			<div class="four columns panel radius">
-				<?php if ( dynamic_sidebar('Home Widget Two')  ) : else : ?>
-					<h5>Recent Posts</h5>
-						<ul>
-						<?php
-						$recent_posts = wp_get_recent_posts();
-						foreach( $recent_posts as $recent ){
-							echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
-							}
-						?>
-						</ul>
-				<?php endif ?>
-			</div>
+				<div class="three columns panel radius">
+					<?php if ( dynamic_sidebar('Sidebar Footer Three')  ) : else : ?>
+						<h5>Recent Posts</h5>
+							<ul>
+							<?php
+							$recent_posts = wp_get_recent_posts();
+							foreach( $recent_posts as $recent ){
+								echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
+								}
+							?>
+							</ul>
+					<?php endif ?>
+				</div>
+				
+				<div class="three columns panel radius">
+					<?php if ( dynamic_sidebar('Sidebar Footer Four')  ) : else : ?>
+						<h5>Recent Posts</h5>
+							<ul>
+							<?php
+							$recent_posts = wp_get_recent_posts();
+							foreach( $recent_posts as $recent ){
+								echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
+								}
+							?>
+							</ul>
+					<?php endif ?>
+				</div>
 		</div>
 	
 
 
-		<?php }?>
+	
 	
 			<div class="row">
 				<div class="eight columns centered">
