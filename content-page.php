6947d0a4bfcb4214726d826adc0a3226809d71bf
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-02-12 00:55:03 -0500

    float thumb left

--- content-page.php
+++ content-page.php
@@ -10,14 +10,16 @@
  */
 ?>
 
-<article class="twelve columns panel callout radius" style="background:#D8D2B1; border:1px solid #799E65;">
+<article>
 
 	<header>
 		<h2><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'foundation' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 	</header>
 
 	<?php if ( has_post_thumbnail()) : ?>
-	<a href="<?php the_permalink(); ?>" class="th" title="<?php the_title_attribute(); ?>" ><?php the_post_thumbnail(); ?></a>
+	<div class="post-thumb">
+		<a href="<?php the_permalink(); ?>" class="th" title="<?php the_title_attribute(); ?>" ><?php the_post_thumbnail(); ?></a>
+	</div>
 	<?php endif; ?>
 	<span style="color:#24221f;">
 		<?php the_content(); ?>
