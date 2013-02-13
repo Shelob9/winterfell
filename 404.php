cceafeb14934a8cecff8208d18c2e591133a914a
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-02-13 14:52:00 -0500

    fixed 404 style/ sidebar position

--- 404.php
+++ 404.php
@@ -1,10 +1,14 @@
 <?php get_header(' '); ?>
-   <div class="nine columns panel callout radius">
-     
-     <h2 class="center">The Page You Requested Can Not Be Found</h2>
-     <h5 class="center"><em>Winter is coming</em></h5>
-     <p>Please contact <a href="mailto:<?php bloginfo('email'); ?>">the webmaster</a> to remedy the situation.</p>
-     
-   </div>
-<?php get_sidebar(); ?>
+<div class="three columns panel sidebar" style="float:right;">
+			<?php get_sidebar() ?>
+</div>
+<div class="nine columns panel callout radius" role="content"  style="background:#D8D2B1; border:1px solid #799E65;">
+ 
+	 <h2 class="center">The Page You Requested Can Not Be Found</h2>
+	 <h5 class="center"><em>Winter is coming</em></h5>
+	 <p>Please contact <a href="mailto:<?php bloginfo('email'); ?>">the webmaster</a> to remedy the situation.</p>
+ 
+</div>
+
+
 <?php get_footer(); ?>
