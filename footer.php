7c7a0236bb67f64245d440f654d6755ed96c38f2
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-23 21:11:15 -0400

    removed footer widgets that looked like shit

--- footer.php
+++ footer.php
@@ -18,60 +18,7 @@
 
 <!-- Footer -->
 <footer>
-	<div class="row">
-		
-		<div class="twelve columns">
-			<div class="row" id="footer-widgets">
-				<div class="three columns panel radius">
-					<?php if ( dynamic_sidebar('Sidebar Footer One')  ) : else : ?>
-						<h5>Pages</h5>
-						<ul>
-						<?php wp_list_pages('title_li='); ?>
-						</ul>
-					<?php endif ?>
-				</div>
 	
-				<div class="three columns panel radius">
-					<?php if ( dynamic_sidebar('Sidebar Footer Two')  ) : else : ?>
-						<h5>Tags</h5>
-						<?php
-						if(get_the_tag_list()) {
-							echo get_the_tag_list('<p><span class="secondary label radius">','</span>&nbsp;<span class="secondary label radius">','</span></p>');
-						}
-						?>
-					<?php endif ?>
-				</div>
-
-				<div class="three columns panel radius">
-					<?php if ( dynamic_sidebar('Sidebar Footer Three')  ) : else : ?>
-						<h5>Recent Posts</h5>
-							<ul>
-							<?php
-							$recent_posts = wp_get_recent_posts();
-							foreach( $recent_posts as $recent ){
-								echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
-								}
-							?>
-							</ul>
-					<?php endif ?>
-				</div>
-				
-				<div class="three columns panel radius">
-					<?php if ( dynamic_sidebar('Sidebar Footer Four')  ) : else : ?>
-						<h5>Recent Posts</h5>
-							<ul>
-							<?php
-							$recent_posts = wp_get_recent_posts();
-							foreach( $recent_posts as $recent ){
-								echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
-								}
-							?>
-							</ul>
-					<?php endif ?>
-				</div>
-			</div>
-		</div>
-	</div>
 	<div class="row">
 		<div class="eight columns centered">
 			<?php echo wf_copyright(); ?> by
--- functions.php
+++ functions.php
@@ -94,52 +94,6 @@
 				'before_title' => '<h5>',
 				'after_title' => '</h5>',
 			) );
-//Main Footer Sidebar
-	// Sidebar Footer Column One
-		register_sidebar( array(
-				'id' => 'foundation_sidebar_footer_one',
-				'name' => __( 'Sidebar Footer One', 'foundation' ),
-				'description' => __( 'This sidebar is located in column one of your theme footer.', 'foundation' ),
-				'before_widget' => '<div id="footer-sidebar">',
-				'after_widget' => '</div>',
-				'before_title' => '<h5>',
-				'after_title' => '</h5>',
-			) );
-
-	// Sidebar Footer Column Two
-		register_sidebar( array(
-				'id' => 'foundation_sidebar_footer_two',
-				'name' => __( 'Sidebar Footer Two', 'foundation' ),
-				'description' => __( 'This sidebar is located in column two of your theme footer.', 'foundation' ),
-				'before_widget' => '<div id="footer-sidebar">',
-				'after_widget' => '</div>',
-				'before_title' => '<h5>',
-				'after_title' => '</h5>',
-			) );
-
-	// Sidebar Footer Column Three
-			register_sidebar( array(
-				'id' => 'foundation_sidebar_footer_three',
-				'name' => __( 'Sidebar Footer Three', 'foundation' ),
-				'description' => __( 'This sidebar is located in column three of your theme footer.', 'foundation' ),
-				'before_widget' => '<div>',
-				'after_widget' => '</div>',
-				'before_title' => '<h5>',
-				'after_title' => '</h5>',
-			) );
-
-	// Sidebar Footer Column Four
-			register_sidebar( array(
-				'id' => 'foundation_sidebar_footer_four',
-				'name' => __( 'Sidebar Footer Four', 'foundation' ),
-				'description' => __( 'This sidebar is located in column four of your theme footer.', 'foundation' ),
-				'before_widget' => '<div>',
-				'after_widget' => '</div>',
-				'before_title' => '<h5>',
-				'after_title' => '</h5>',
-			) );
-		}
-
 	add_action( 'widgets_init', 'foundation_widgets' );
 
 /**
