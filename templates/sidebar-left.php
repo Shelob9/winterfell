28ff375e867d2fb01219e85e44314884b7b4bb32
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-25 00:37:15 -0400

    row for sidebar-left temp

--- templates/sidebar-left.php
+++ templates/sidebar-left.php
@@ -5,22 +5,23 @@
  */
 
 get_header(); ?>
-	<div class="three columns panel">
-		<?php get_sidebar(); ?>
-	</div>
-    <!-- Main Content -->
-    <div class="nine columns panel callout radius" role="content"  style="background:#D8D2B1; border:1px solid #799E65;">
+	<div class="row">
+		<div class="three columns panel">
+			<?php get_sidebar(); ?>
+		</div>
+		<!-- Main Content -->
+		<div class="nine columns panel callout radius" role="content"  style="background:#D8D2B1; border:1px solid #799E65;">
 
-		<?php if ( have_posts() ) : ?>
+			<?php if ( have_posts() ) : ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', 'page' ); ?>
-			<?php endwhile; ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'content', 'page' ); ?>
+				<?php endwhile; ?>
 			
-		<?php endif; ?>
+			<?php endif; ?>
 
-    </div>
-    <!-- End Main Content -->
-	
+		</div>
+		<!-- End Main Content -->
+	</div>
 </div>
 <?php get_footer(); ?>
