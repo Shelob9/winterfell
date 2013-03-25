4bd70d7343d91f8a2e0a3eae0e87c57d2ca5edc5
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-25 00:42:44 -0400

    pull-nine

--- templates/sidebar-left.php
+++ templates/sidebar-left.php
@@ -5,24 +5,21 @@
  */
 
 get_header(); ?>
-</div>
-	<div class="row">
-		<div class="three columns panel">
-			<?php get_sidebar(); ?>
-		</div>
-		<!-- Main Content -->
-		<div class="nine columns panel callout radius" role="content"  style="background:#D8D2B1; border:1px solid #799E65;">
+    <!-- Main Content -->
+    <div class="nine columns panel callout radius" role="content"  style="background:#D8D2B1; border:1px solid #799E65;">
 
-			<?php if ( have_posts() ) : ?>
+		<?php if ( have_posts() ) : ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php get_template_part( 'content', 'page' ); ?>
-				<?php endwhile; ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
+			<?php endwhile; ?>
 			
-			<?php endif; ?>
+		<?php endif; ?>
 
-		</div>
-		<!-- End Main Content -->
+    </div>
+    <!-- End Main Content -->
+	<div class="three columns panel pull-nine">
+		<?php get_sidebar(); ?>
 	</div>
 </div>
 <?php get_footer(); ?>
