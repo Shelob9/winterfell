cfcfd7b60aee4ac4981150846a45201cfedb028d
Josh Pollock <josh@Josh-Pollocks-MacBook-Pro.local>
2013-03-25 00:18:59 -0400

    credit links in footer

--- footer.php
+++ footer.php
@@ -21,6 +21,8 @@
 	
 	<div class="row">
 		<div class="eight columns centered">
+		<a href="<?php echo esc_url( __( 'http://wordpress.org/') ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform'); ?>"><?php printf( __( 'Powered by %s' ), 'WordPress,' ); ?></a>
+		<a href="<?php echo esc_url( __( 'http://ComplexWaveform.com/jp?Winterfell/') ); ?>" title="<?php esc_attr_e( 'Winterfell WordPress Theme by Josh Pollock'); ?>"><?php printf( __( 'using the by %s' ), 'Winterfell Theme.' ); ?></a>
 			<?php echo wf_copyright(); ?> by
 			<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>.
 		</div>
